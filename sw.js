3ef2283219190774218f084cea0a76e9397e3728
aufthority <aufali@gmail.com>
2026-04-08 11:31:56 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    — Firebase/Fonts cached for offline use
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'neurohabit-v35';
+const CACHE_NAME = 'neurohabit-v36';
 
 const PRECACHE = [
   '/neurohabitbeta/manifest.json',
